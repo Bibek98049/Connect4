622f02bc00ff23bd7ad0e6202a138081016805fd
Bibek Luitel <87264489+Bibek98049@users.noreply.github.com>
2022-04-05 11:44:49 +1000

    Add files via upload

---
+++ GameTree.h
@@ -0,0 +1,47 @@
+
+
+// A tree of Nodes with Connect 4 game states
+
+#ifndef GAMETREE_H
+#define GAMETREE_H
+#include "Node.h"
+
+class GameTree
+{
+public:
+    GameTree(Connect4 * game)
+    {
+        Node * node = new Node(game);
+        _root = node;
+    }
+    void reinitialize(Connect4 * game)
+    {
+        delete _root;
+        Node * node = new Node(game);
+        _root = node;
+    }
+
+    int best_move() const
+    {
+        return _root->minimax();
+    }
+    ~GameTree()
+    {
+        delete _root;
+    }
+
+    friend std::ostream & operator<< (std::ostream & cout, const GameTree & tree)
+    {
+        cout << "<Tree " << &tree << " _root: " << tree._root << " >\n";
+        if (tree._root != NULL)
+        {
+            cout << "priniting games....\n\n";
+            cout << *tree._root;
+        }
+        return cout;
+    }
+private:
+    Node * _root;
+};
+
+#endif
